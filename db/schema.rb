70afc8163a5120addc6b0ea6960aa4a71a46875d
Scott Kaplan <scott@kaplans.com>
2014-09-25 23:29:39 +0000

    add accounts.active

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901045452) do
+ActiveRecord::Schema.define(version: 20140919151657) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140901045452) do
     t.string   "mobile"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "active"
   end
 
   add_index "accounts", ["email"], name: "email", unique: true, using: :btree
