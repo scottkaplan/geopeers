21b8025d6dd53c5dceeb2492d8492cd484cd169f
Scott Kaplan <scott@kaplans.com>
2014-10-05 19:38:15 +0000

    factor code

--- public/db.js
+++ public/db.js
@@ -2,26 +2,39 @@ var db = {
     // this is only supported in phonegap, not web app
     handle: null,
     init: function () {
+	if (db.handle) {
+	    return;
+	}
 	db.handle = window.sqlitePlugin.openDatabase({name: "geopeers.db"});
 	db.handle.transaction(function(tx) {
-		tx.executeSql('DROP TABLE IF EXISTS globals');
 		tx.executeSql('CREATE TABLE IF NOT EXISTS globals ("key" text unique, value text)');
 	    });
+    },
+    reset: function () {
+	db.handle.transaction(function(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS globals');
+	    }, db.init())
     },
     get_global: function (key, callback) {
 	db.handle.transaction(function (tx) {
 		tx.executeSql('SELECT value FROM globals WHERE "key" = ?', [key], function(tx, response) {
-			callback (response);
+			callback (tx, response);
 		    },
 		    db.error_callback);
 	    });
     },
     set_global: function (key, value) {
 	db.handle.transaction(function (tx) {
-		tx.executeSql('REPLACE INTO globals ("key", value) VALUES (?,?)', [key, value], null , db.error_callback);
+		tx.executeSql('REPLACE INTO globals ("key", value) VALUES (?,?)', [key, value], set_callback , db.error_callback);
 	    });
     },
+    set_callback: function  (tx, results) {
+	console.log (tx);
+	console.log (results);
+	console.log (results.rows);
+    },
     error_callback: function (err) {
+	console.log (err);
 	console.log (err.message);
     },
 };
