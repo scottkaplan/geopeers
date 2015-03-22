51e0bd81fa34582586f27338c30da3d7b0a59a75
Scott Kaplan <scott@kaplans.com>
2015-03-22 16:39:43 +0000

    Generalized routine to run CloudFormation file

--- tools/amazon.rb
+++ tools/amazon.rb
@@ -75,7 +75,30 @@ def launch_ami(ami_id,
   end
 end
 
-err = launch_ami ("ami-ff14f3bb")
-if err
-  puts err
+def run_cloudformation cf_file
+  region = 'us-west-2'
+  stack_name = File.basename(cf_file, ".*")
+  stack_name.gsub!(/_/,'-')
+  begin
+    cloudformation = Aws::CloudFormation::Client.new(region: region)
+    resp = cloudformation.create_stack(
+      stack_name: stack_name,
+      template_body: File.read(cf_file),
+      disable_rollback: true,
+    )
+    puts resp.stack_id
+  rescue Aws::EC2::Errors::ServiceError => e
+    return e
+  end
+
 end
+
+if ARGV[0]
+  err = run_cloudformation (ARGV[0])
+  if err
+    puts err
+  end
+else
+  puts "Please supply the name of the CF JSON file"
+end
+# err = launch_ami ("ami-ff14f3bb")
